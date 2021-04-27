45873a9875cedb8369fe978373755434c52a4cc7
남승균 <skyun.nam@gmail.com>
2021-04-27 16:10:44 +0900

    Initial Commit

--- app/src/main/java/com/example/basicwidget/MainActivity.java
+++ app/src/main/java/com/example/basicwidget/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.basicwidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private View textView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        //  btnVisible, btnGone, btnInvisible
+        findViewById(R.id.btnVisible).setOnClickListener(this);
+        findViewById(R.id.btnGone).setOnClickListener(this);
+        findViewById(R.id.btnInvisible).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int buttonId = v.getId();
+
+        if (buttonId == R.id.btnVisible) {
+            textView.setVisibility(View.VISIBLE);
+        } else if (buttonId == R.id.btnGone) {
+            textView.setVisibility(View.GONE);
+        } else if (buttonId == R.id.btnInvisible) {
+            textView.setVisibility(View.INVISIBLE);
+        }
     }
 }
